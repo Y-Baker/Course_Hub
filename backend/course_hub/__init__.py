a48fa42fbd00e6c64a1958b804f5f8900cae8482
Yousef <yuossefbakier@gmail.com>
2023-11-11 16:29:23 +0200

    Update Init (app)

--- backend/course_hub/__init__.py
+++ backend/course_hub/__init__.py
@@ -3,10 +3,9 @@
 
 
 from flask import Flask
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 
 app.config.from_object('config.default')
 
-app.config.from_pyfile('config.py')
 
 app.config.from_envvar('APP_CONFIG_FILE')
--- backend/start.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export APP_CONFIG_FILE=/root/alx/Course_Hub/backend/config/development.py
-python app.py
